5d81fc964cc5ef62b9ed76d2c6aca2773c6e81c4
Taner Saydam <83103843+TanerSaydam@users.noreply.github.com>
2026-04-06 08:44:21 +0300

    Refactor Entity class equality methods

--- DomainDrivenDesignUdemy.Domain/Abstractions/Entity.cs
+++ DomainDrivenDesignUdemy.Domain/Abstractions/Entity.cs
@@ -2,53 +2,42 @@
 public abstract class Entity : IEquatable<Entity>
 {
     public Guid Id { get; init; }
+
     public Entity(Guid id)
     {
         Id = id;
     }
 
-    public override bool Equals(object? obj)
-    {
-        if(obj is null)
-        {
-            return false;
-        }
+    public override bool Equals(object? obj) 
+        => obj is Entity entity && Equals(entity);
 
-        if(obj is not Entity entity)
-        {
-            return false;
-        }
-
-        if(obj.GetType() != GetType())
-        {
-            return false;
-        }
-
-        return entity.Id == Id;
-    }
-
-    public override int GetHashCode()
-    {
-        return Id.GetHashCode();
-    }
+    public override int GetHashCode() 
+        => Id.GetHashCode();
 
     public bool Equals(Entity? other)
     {
         if (other is null)
-        {
             return false;
-        }
-
-        if (other is not Entity entity)
-        {
-            return false;
-        }
 
         if (other.GetType() != GetType())
-        {
             return false;
-        }
 
-        return entity.Id == Id;
+        return other.Id == Id;
+    }
+
+    public static bool operator ==(Entity? left, Entity? right)
+    {
+        if (left is null && right is null)
+            return true;
+
+        if (left is null || right is null)
+            return false;
+
+        return left.Equals(right);
+    }
+
+    public static bool operator !=(Entity? left, Entity? right)
+    {
+        return !(left == right);
     }
 }
